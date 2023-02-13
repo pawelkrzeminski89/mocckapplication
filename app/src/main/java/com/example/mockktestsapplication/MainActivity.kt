3613ee75ffa51d0391d4ddac89500c6f3ca151d0
pawelkrzeminski1989 <108516078+pawelkrzeminski1989@users.noreply.github.com>
2023-02-13 12:45:33 +0100

    add simple composable screen

--- app/src/main/java/com/example/mockktestsapplication/MainActivity.kt
+++ app/src/main/java/com/example/mockktestsapplication/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.mockktestsapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mockktestsapplication.ui.theme.MockkTestsApplicationTheme
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CurrentDateForm()
                 }
             }
         }
@@ -30,14 +37,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CurrentDateForm() {
+    var date = remember { Calendar.getInstance().time.time }
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        TextField(
+            value = date.toString(),
+            onValueChange = { newDate ->
+                // tutaj należy dodać kod do konwersji tekstu na datę i ustawienia jej w zmiennej date
+            }
+        )
+        Button(onClick = { /* tutaj możesz dodać logikę związaną z potwierdzeniem daty */ }) {
+            Text("Zatwierdź")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MockkTestsApplicationTheme {
-        Greeting("Android")
+        CurrentDateForm()
     }
 }
